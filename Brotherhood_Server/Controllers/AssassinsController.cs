014beefa5859afc1c1024809c4c7bf1a302a250b
2156153 <m.gagnon941@gmail.com>
2022-11-17 16:08:20 -0500

    feat: Configure user registration

---
+++ Brotherhood_Server/Controllers/AssassinsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Brotherhood_Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brotherhood_Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AssassinsController : ControllerBase
+	{
+		UserManager<Assassin> _UserManager;
+
+		public AssassinsController(UserManager<Assassin> userManager)
+		{
+			_UserManager = userManager;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterDTO register)
+		{
+			if (register.Password != register.PasswordConfirm)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest, 
+					new { Message = "Passwords don't match." });
+			}
+
+			Assassin assassin = new Assassin()
+			{
+				UserName = register.UserName,
+				Email = register.Email
+			};
+
+			IdentityResult result = await _UserManager.CreateAsync(assassin, register.Password);
+			if (!result.Succeeded)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError,
+					new { Message = "Could not register assassin." });
+			}
+
+			return Ok();
+		}
+	}
+}
---
+++ Brotherhood_Server/Models/RegisterDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Brotherhood_Server.Models
+{
+	public class RegisterDTO
+	{
+		[Required]
+		public string UserName { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+		[Required]
+		public string PasswordConfirm { get; set; }
+	}
+}
