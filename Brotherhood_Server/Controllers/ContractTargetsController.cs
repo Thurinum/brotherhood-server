cd84da1868a8854a2cb13bb83eeb2efc260660fe
2156153 <m.gagnon941@gmail.com>
2022-11-29 10:04:52 -0500

    feat: Create new contract target

--- Brotherhood_Server/Controllers/ContractTargetsController.cs
+++ Brotherhood_Server/Controllers/ContractTargetsController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Brotherhood_Server.Data;
 using Brotherhood_Server.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace Brotherhood_Server.Controllers
 {
@@ -14,12 +17,26 @@ namespace Brotherhood_Server.Controllers
     [ApiController]
     public class ContractTargetsController : ControllerBase
     {
-        private readonly BrotherhoodServerContext _context;
+		private readonly BrotherhoodServerContext _context;
+		private readonly UserManager<Assassin> _userManager;
 
-        public ContractTargetsController(BrotherhoodServerContext context)
-        {
-            _context = context;
-        }
+		public ContractTargetsController(BrotherhoodServerContext context, UserManager<Assassin> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
 
-    }
+		[HttpPost]
+		[Authorize]
+		[Route("contracts/target/create")]
+		public async Task<ActionResult<ContractTarget>> CreateContractTarget(ContractTarget target)
+		{
+			_context.Targets.Add(target);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("CreateContractTarget", new { id = target.Id }, target);
+		}
+
+		private async Task<Assassin> GetCurrentUser() => await _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+	}
 }
