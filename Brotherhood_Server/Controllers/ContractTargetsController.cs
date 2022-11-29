da7a6b5a528be7249976ca910f41c94117c74f26
2156153 <m.gagnon941@gmail.com>
2022-11-29 09:20:54 -0500

    temp: Create ContractTargetsController

---
+++ Brotherhood_Server/Controllers/ContractTargetsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Brotherhood_Server.Data;
+using Brotherhood_Server.Models;
+
+namespace Brotherhood_Server.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ContractTargetsController : ControllerBase
+    {
+        private readonly BrotherhoodServerContext _context;
+
+        public ContractTargetsController(BrotherhoodServerContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
