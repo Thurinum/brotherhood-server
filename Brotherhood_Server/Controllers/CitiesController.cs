7c9024427e4151d954eacb5b8e32519c95943da3
Thurinum <m.gagnon941@gmail.com>
2022-12-04 13:34:10 -0500

    feat: Require auth for cities

--- Brotherhood_Server/Controllers/CitiesController.cs
+++ Brotherhood_Server/Controllers/CitiesController.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Brotherhood_Server.Data;
 using Brotherhood_Server.Models;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 
 namespace Brotherhood_Server.Controllers
 {
 	[ApiController]
+	[Authorize]
 	[Route("api")]
 	public class CitiesController : ControllerBase
 	{
